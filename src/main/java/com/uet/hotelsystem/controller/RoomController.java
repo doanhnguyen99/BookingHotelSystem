b18eebf6aa1a8bb07ad9876d22e479ca40e4073c
doanhnguyen99 <doanh080199@gmail.com>
2021-03-12 00:55:59 +0700

    room pagination

--- src/main/java/com/uet/hotelsystem/controller/RoomController.java
+++ src/main/java/com/uet/hotelsystem/controller/RoomController.java
@@ -3,6 +3,7 @@ package com.uet.hotelsystem.controller;
 import com.uet.hotelsystem.model.RoomEntity;
 import com.uet.hotelsystem.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,22 +17,33 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
 
-    @GetMapping("/all")
-    public String viewAllRoom(Model model){
-        List<RoomEntity> roomEntityList = roomService.listAll();
-        model.addAttribute("listRooms", roomEntityList);
+    @RequestMapping("/all")
+    public String viewHomePage(Model model) {
+        return viewPage(model, 1);
+    }
+
+    @RequestMapping("/page/{pageNum}")
+    public String viewPage(Model model,
+                           @PathVariable(name = "pageNum") int pageNum) {
+
+        Page<RoomEntity> page = roomService.listAll(pageNum);
+        List<RoomEntity> listRooms = page.getContent();
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("listRooms", listRooms);
         return "room-management";
     }
 
     @RequestMapping("/new")
-    public String showNewProductPage(Model model) {
+    public String showNewRoomPage(Model model) {
         RoomEntity roomEntity = new RoomEntity();
         model.addAttribute("room", roomEntity);
         return "new_room";
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String saveProduct(@ModelAttribute("room") RoomEntity roomEntity) {
+    public String saveRoom(@ModelAttribute("room") RoomEntity roomEntity) {
         roomService.save(roomEntity);
         return "redirect:/room/all";
     }
@@ -45,9 +57,8 @@ public class RoomController {
     }
 
     @RequestMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable(name = "id") Long id) {
+    public String deleteRoom(@PathVariable(name = "id") Long id) {
         roomService.deleteRoom(id);
         return "redirect:/room/all";
     }
-
 }
--- src/main/java/com/uet/hotelsystem/service/RoomService.java
+++ src/main/java/com/uet/hotelsystem/service/RoomService.java
@@ -3,6 +3,9 @@ package com.uet.hotelsystem.service;
 import com.uet.hotelsystem.model.RoomEntity;
 import com.uet.hotelsystem.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,7 +17,13 @@ public class RoomService {
     @Autowired
     private RoomRepository roomRepository;
 
-    public List<RoomEntity> listAll(){
+    public Page<RoomEntity> listAll(int pageNum) {
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+        return roomRepository.findAll(pageable);
+    }
+
+    public List<RoomEntity> getAll(){
         return roomRepository.findAll();
     }
 
